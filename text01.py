655cc3756d0cf94915f3131f7d246d60a118b63d
Gabriel Santos <gabrielplayon007@gmail.com>
2022-12-15 21:43:38 -0300

    1 commit

---
+++ text01.py
@@ -0,0 +1,17 @@
+nome = input ('Qual é seu nome? ')
+idade = input ('Qual é sua idade? ')
+peso = input ('Qual é seu peso? ')
+print(nome, idade, peso)
+#01
+name = input ('Qual o seu nome? ')
+print ('Olá', nome,'! Prazer em te conhecer!')
+#02
+day = input('Dia =')
+month = input('Mês =')
+year = input('Ano =')
+print('Você nasceu no dia', day, 'de', month, 'de', year, '. Correto?')
+#03
+a = int(input('Primeiro número:'))
+b = int(input('Segundo número:'))
+soma = a + b
+print('A soma é:', soma)
