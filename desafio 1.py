07e9bdc79ffe2c5ef1fb90a985e223c6790047f2
Gabriel Santos <119314794+gabrielsants7@users.noreply.github.com>
2022-12-28 15:30:41 -0300

    Desafios
    Eu fiz os desafios 1, 2 e 3.

---
+++ desafio 1.py
@@ -0,0 +1,13 @@
+#1
+
+nome = input ('Qual o seu nome?')
+print('Olá', nome,'! é um prazer te conhecer!')
+
+'''
+Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
+'''
+
+
+
+
+
---
+++ desafio 3.py
@@ -0,0 +1,13 @@
+#03
+a = int(input('Primeiro número:'))
+b = int(input('Segundo número:'))
+soma = a + b
+print('A soma é:', soma)
+
+'''
+
+crie um script Python que leia dois número e tente mostrar a soma entre eles
+
+'''
+
+
