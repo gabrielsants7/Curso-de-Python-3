27cc9151d433aaa0806e5c126c77ae4d4c93ea59
Gabriel Santos <119314794+gabrielsants7@users.noreply.github.com>
2023-01-13 20:37:20 -0300

    Update exercício 44.py
    obs: esqueci de colocar o comentário

--- exercício 44.py
+++ exercício 44.py
@@ -26,3 +26,12 @@ else:
     print('OPÇÃO INVÁLIDA de pagamento. Tente novamente')
     print(f'Sua compra de R${preço:.2f} vai custar R${total:.2f} no final.')
 
+'''
+Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição 
+de pagamento:
+
+à vista dinheiro/cheque: 10% de desconto
+à vista no cartão: 5% de desconto
+em até 2x no cartão: preço formal 
+3x ou mais no cartão: 20% de juros
+'''
